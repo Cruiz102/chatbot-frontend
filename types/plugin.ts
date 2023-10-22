4cc41e3d2fcdaf47a4b0acc4c2bf498fd7062b1d
Cruiz102 <cruiznavarro44@gmail.com>
2023-10-21 23:52:54 -0400

    added weaviate interfaces

--- types/plugin.ts
+++ types/plugin.ts
@@ -13,11 +13,29 @@ export interface PluginKey {
 
 export enum PluginID {
   GOOGLE_SEARCH = 'google-search',
+  WEAVIATE_SEARCH = 'weaviate-search',  // New enum value for Weaviate
 }
 
 export enum PluginName {
   GOOGLE_SEARCH = 'Google Search',
+  WEAVIATE_SEARCH = 'Weaviate Search',  // New enum value for Weaviate
 }
+
+// New interface for Weaviate credentials
+export interface WeaviateCredentials {
+  weaviateUrl: string;
+  weaviateApiKey: string;
+}
+
+// Existing or new union type for all plugins
+export type AnyPlugin = Plugin | WeaviatePlugin;  // Add WeaviatePlugin to the union
+
+// New type for Plugin with Weaviate
+export type WeaviatePlugin = {
+  id: PluginID.WEAVIATE_SEARCH;
+  name: PluginName.WEAVIATE_SEARCH;
+  credentials: WeaviateCredentials;
+};
 
 export const Plugins: Record<PluginID, Plugin> = {
   [PluginID.GOOGLE_SEARCH]: {
@@ -30,6 +48,20 @@ export const Plugins: Record<PluginID, Plugin> = {
       },
       {
         key: 'GOOGLE_CSE_ID',
+        value: '',
+      },
+    ],
+  },
+  [PluginID.WEAVIATE_SEARCH]: {  // New entry for Weaviate
+    id: PluginID.WEAVIATE_SEARCH,
+    name: PluginName.WEAVIATE_SEARCH,
+    requiredKeys: [
+      {
+        key: 'WEAVIATE_URL',
+        value: '',
+      },
+      {
+        key: 'WEAVIATE_API_KEY',
         value: '',
       },
     ],
